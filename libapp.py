28fb258b8e30ada787f7dbffe3d27ab11d15ea19
Mbarak Mbigo <mbigombaraktram@gmail.com>
2017-01-25 10:22:24 +0300

    Added Some styling

--- libapp.py
+++ libapp.py
@@ -1,10 +1,10 @@
 #demo run before final structure
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def initialize():
-	return "Root of M-Library Application"
+	return render_template('signin.html')
 
 if __name__ == '__main__':
 	app.run()
